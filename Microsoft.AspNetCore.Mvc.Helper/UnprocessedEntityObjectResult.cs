0354c8a4409c5b9856e0e189a07e0aa97f22abc0
c4bo3l <c4bo3l@gmail.com>
2018-02-12 22:59:47 +0800

    [+] Unprocessed Entity Object action result.

---
+++ Microsoft.AspNetCore.Mvc.Helper/UnprocessedEntityObjectResult.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+
+namespace Microsoft.AspNetCore.Mvc.Helper
+{
+    public class UnprocessedEntityObjectResult : ObjectResult
+    {
+        public UnprocessedEntityObjectResult(ModelStateDictionary error) 
+            : base(new SerializableError(error))
+        {
+            if (error == null)
+                throw new ArgumentNullException();
+            StatusCode = 422;
+        }
+    }
+}
